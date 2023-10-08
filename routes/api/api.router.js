3e6813820b77df3676dc1d41ef1ee75cef2cd891
benkortbi <new394665@gmail.com>
2023-10-08 15:34:34 +0100

    new changes

--- routes/api/api.router.js
+++ routes/api/api.router.js
@@ -8,25 +8,8 @@ const {
 } = require("../../controllers/tasks.controller");
 
 const apiRouter = express.Router();
-apiRouter.get("/", (_, res) => {
-	const express = require("express");
-	const {
-		getTasks,
-		postTask,
-		deleteTask,
-		getSingleTask,
-		updateTask,
-	} = require("../../controllers/tasks.controller");
-
-	const apiRouter = express.Router();
-
-	apiRouter.get("/tasks", getTasks);
-	apiRouter.get("/tasks/:id", getSingleTask);
-	apiRouter.post("/new-task", postTask);
-	apiRouter.delete("/tasks/:id", deleteTask);
-	apiRouter.patch("/tasks/:id", updateTask);
-
-	module.exports = apiRouter;
+apiRouter.get("/", (req, res) => {
+	res.status(200).json({ message: "this is a message" });
 });
 apiRouter.get("/tasks", getTasks);
 apiRouter.get("/tasks/:id", getSingleTask);

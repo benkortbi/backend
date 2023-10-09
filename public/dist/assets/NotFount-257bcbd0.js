5e5681873dd9a0d5bc352b66e27c94751c9a9623
benkortbi <new394665@gmail.com>
2023-10-09 21:38:31 +0100

    edite api

--- public/dist/assets/NotFount-257bcbd0.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-07c87fb5.js";function n(){return t.jsx("div",{className:"w-full h-screen grid place-content-center text-white",children:"404 | NOT FOUND"})}export{n as default};

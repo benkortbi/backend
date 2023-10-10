93f4bc775945461f58b58cd72128af8692721c71
benkortbi <new394665@gmail.com>
2023-10-10 14:45:05 +0100

    New Changes!

---
+++ public/dist/assets/NotFount-1d9e9dfb.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-7bf4c6f9.js";function n(){return t.jsx("div",{className:"w-full h-screen grid place-content-center text-white",children:"404 | NOT FOUND"})}export{n as default};
